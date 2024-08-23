58beb805715d3c01cc61211cd5a54bccbbdcf915
Younes Id Talab <113123436+younesidt@users.noreply.github.com>
2024-08-23 22:13:35 +0100

    ttt

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import NavBar from '../src/components/NavBar.vue'
 </script>
 
 <template>
-  <div class="bg-[#171717] min-h-screen flex flex-col items-center py-5 font-Poppins">
+  <div class="bg-[#171717] dark:bg-[#171717] min-h-screen flex flex-col items-center py-5 font-Poppins">
     <NavBar />
     <router-view />
   </div>
